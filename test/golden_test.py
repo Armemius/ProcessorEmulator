aa85ae71e3bb6e21064679f4431712d92159cb9e
Armemius <rusgy@yandex.ru>
2024-09-27 20:51:58 +0300

    test pipeline

--- test/golden_test.py
+++ test/golden_test.py
@@ -2,11 +2,11 @@ import pytest
 from src.emulator.emulator import main as emulator_main
 
 def test_prob5():
-    emulator_main("sources/prob5.opc")
+    emulator_main("test/sources/prob5.opc")
     with open('states.txt', 'r') as golden_file:
        output = golden_file.read().strip()
 
-    with open('output/prob5.txt', 'r') as test_file:
+    with open('test/output/prob5.txt', 'r') as test_file:
        expected = test_file.read().strip()
 
     assert expected == output
