7bee14a4f4266ae6a3e6716c7bb7a7643973d012
Armemius <rusgy@yandex.ru>
2024-09-29 00:43:05 +0300

    modify tests & python reformat

--- test/golden_test.py
+++ test/golden_test.py
@@ -2,10 +2,11 @@ import pytest
 
 from src.emulator.emulator import main as emulator_main
 
+
 @pytest.mark.timeout(5)
 def test_prob5():
     emulator_main('test/sources/prob5.opc')
-    with open('test/sources.txt', 'r') as golden_file:
+    with open('sources.txt', 'r') as golden_file:
         output = golden_file.read()
 
     with open('test/output/prob5.txt', 'r') as test_file:
